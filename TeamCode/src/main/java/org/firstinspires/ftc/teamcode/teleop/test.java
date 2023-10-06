4858ff83e4716600d93a81b8f27e8bced113cc4c
MelaLin <melalin05@gmail.com>
2023-10-06 16:45:47 -0400

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/test.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ServoControllerEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name = "test", group = "Default")
+
+public class test {
+
+    public class test extends LinearOpMode {
+        private DcMotor motor;
+
+        @Override
+        public void runOpMode(){
+            motor = hardwareMap.dcMotor.get("Motor3");
+
+            waitForStart();
+
+            if (opModeIsActive()){
+                motor.setPower(-0.36);
+            }
+        }
+    }
+}
+
+}
