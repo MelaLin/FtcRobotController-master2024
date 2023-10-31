0d59d2e24fa285ead1e2b8398722e07696f67a94
Isabelle Tardivon <isabelletardivon@yahoo.com>
2023-10-31 16:01:03 -0400

    poggers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Controller.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ServoControllerEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name = "Controller", group = "Default")
+
+    public class Controller extends LinearOpMode {
+
+        @Override
+        public void runOpMode() {
+            DcMotor motor = hardwareMap.dcMotor.get("Motor3");
+
+            waitForStart();
+
+            if (opModeIsActive()) {
+
+            }
+        }
+}
