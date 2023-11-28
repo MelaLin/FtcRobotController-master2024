08f414027d9d166b975c945acbbdd80dc657097b
Isabelle Tardivon <isabelletardivon@yahoo.com>
2023-11-28 15:26:25 -0500

    poggers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Controllerv2.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ServoControllerEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name = "Controllerv2", group = "Default")
+
+public class Controllerv2 {
+
+}
