ea090fd561354fd07ff1e2b48dc45d20de2e97c6
Isabelle Tardivon <isabelletardivon@yahoo.com>
2023-12-20 13:44:49 -0500

    pog

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Controller.java
@@ -14,6 +14,17 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 @TeleOp(name = "Controller", group = "Default")
 
     public class Controller extends LinearOpMode {
+    private DcMotorEx frontLeft;
+    private DcMotorEx frontRight;
+    private DcMotorEx backLeft;
+    private DcMotorEx backRight;
+
+    private Double speedLeft = 0.0;
+    private Double speedRight = 0.0;
+    private Double frontLeftVelocity = 0.0;
+    private Double frontRightVelocity = 0.0;
+    private Double backLeftVelocity = 0.0;
+    private Double backRightVelocity = 0.0;
 
         @Override
         public void runOpMode() {
@@ -22,22 +33,127 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
             DcMotor backRight = hardwareMap.dcMotor.get("Motor2");
             DcMotor frontRight = hardwareMap.dcMotor.get("Motor3");
 
+            frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            telemetry.addData("Status", "Initialized");
+            telemetry.update();
+
             waitForStart();
 
-            if (opModeIsActive()) {
+            while (opModeIsActive()) {
 
-                backLeft.setPower(0.7);
-                frontLeft.setPower(0.7);
-                backRight.setPower(0.7);
-                frontRight.setPower(0.7);
-                sleep (1000);
+                telemetry.addData("Status", "Running");
+                telemetry.addData("gamepad1.right_stick_y: ", gamepad1.right_stick_y);
+                telemetry.addData("gamepad1.right_stick_x: ", gamepad1.right_stick_x);
+                telemetry.addData("gamepad1.left_stick_y: ", gamepad1.left_stick_y);
+                telemetry.addData("gamepad1.left_stick_x: ", gamepad1.left_stick_x);
+                telemetry.addData("speedLeft: ", speedLeft);
+                telemetry.addData("speedRight: ", speedRight);
+                /*telemetry.addData("Motor Left Front Speed: ", frontLeft.getVelocity());
+                telemetry.addData("Motor Right Front Speed: ", frontRight.getVelocity());
+                telemetry.addData("Motor Left Back Speed: ", backLeft.getVelocity());
+                telemetry.addData("Motor Right Back Speed: ", backRight.getVelocity());*/
+                telemetry.update();
 
-                backLeft.setPower (0);
-                frontLeft.setPower (0);
-                backRight.setPower (0);
-                frontRight.setPower (0);
-                sleep(500);
+                /*frontLeft.setMotorEnable();
+                frontRight.setMotorEnable();
+                backLeft.setMotorEnable();
+                backRight.setMotorEnable();*/
+
+                speedLeft = 0.0;
+                speedRight = 0.0;
+                frontLeftVelocity = 0.0;
+                frontRightVelocity = 0.0;
+                backLeftVelocity = 0.0;
+                backRightVelocity = 0.0;
+
+                setSpeed();
+                setRotation();
+                setDirection();
+
+
 
             }
         }
+    public void setSpeed() {
+        Double offsetLeft = 1.08;
+        Double offsetRight = 0.92;
+        if ((gamepad1.right_trigger > 0.7) && (gamepad1.left_trigger > 0.7)) { //normal
+            speedLeft = 1250.0 * offsetLeft;
+            speedRight = 1250.0 * offsetRight;
+        } else if (gamepad1.right_trigger > 0.7) { //fast
+            speedLeft = 2000.0 * offsetLeft;
+            speedRight = 2000.0 * offsetRight;
+        } else if (gamepad1.left_trigger > 0.7) { //slow
+            speedLeft = 650.0 * offsetLeft;
+            speedRight = 650.0 * offsetRight;
+        } else { //normal
+            speedLeft = 1250.0 * offsetLeft;
+            speedRight = 1250.0 * offsetRight;
+        }
+    }
+
+    public void setRotation() {
+        if (gamepad1.left_stick_x < -0.5) { //rotate left
+            frontLeftVelocity -= speedLeft / 2;
+            frontRightVelocity += speedRight / 2;
+            backLeftVelocity -= speedLeft / 2;
+            backRightVelocity += speedRight / 2;
+        } else if (gamepad1.left_stick_x > 0.5) { //rotate right
+            frontLeftVelocity += speedLeft / 2;
+            frontRightVelocity -= speedRight / 2;
+            backLeftVelocity += speedLeft / 2;
+            backRightVelocity -= speedRight / 2;
+        }
+    }
+
+    public void setDirection() {
+        if (gamepad1.right_stick_y <= -0.99 && (Math.abs(gamepad1.right_stick_x) < 0.3)) { //forward
+            frontLeftVelocity += speedLeft;
+            frontRightVelocity += speedRight;
+            backLeftVelocity += speedLeft;
+            backRightVelocity += speedRight;
+        } else if (gamepad1.right_stick_y >= 0.99 && (Math.abs(gamepad1.right_stick_x) < 0.3)) { //back
+            frontLeftVelocity -= speedLeft;
+            frontRightVelocity -= speedRight;
+            backLeftVelocity -= speedLeft;
+            backRightVelocity -= speedRight;
+        } else if (gamepad1.right_stick_x <= -0.99 && (Math.abs(gamepad1.right_stick_y) < 0.3)) { //left
+            frontLeftVelocity -= speedLeft;
+            frontRightVelocity += speedRight;
+            backLeftVelocity += speedLeft;
+            backRightVelocity -= speedRight;
+        } else if (gamepad1.right_stick_x >= 0.99 && (Math.abs(gamepad1.right_stick_y) < 0.3)) { //right
+            frontLeftVelocity += speedLeft;
+            frontRightVelocity -= speedRight;
+            backLeftVelocity -= speedLeft;
+            backRightVelocity += speedRight;
+        } else if ((gamepad1.right_stick_y < -0.3) && (gamepad1.right_stick_x < -0.3)) { //forward left
+            frontLeftVelocity += 0;
+            frontRightVelocity += speedRight;
+            backLeftVelocity += speedLeft;
+            backRightVelocity += 0;
+        } else if ((gamepad1.right_stick_y < -0.3) && (gamepad1.right_stick_x > 0.3)) { //forward right
+            frontLeftVelocity += speedLeft;
+            frontRightVelocity += 0;
+            backLeftVelocity += 0;
+            backRightVelocity += speedRight;
+        } else if ((gamepad1.right_stick_y > 0.3) && (gamepad1.right_stick_x < -0.3)) { //back left
+            frontLeftVelocity -= speedLeft;
+            frontRightVelocity += 0;
+            backLeftVelocity += 0;
+            backRightVelocity -= speedRight;
+        } else if ((gamepad1.right_stick_y > 0.3) && (gamepad1.right_stick_x > 0.3)) { //back right
+            frontLeftVelocity += 0;
+            frontRightVelocity -= speedRight;
+            backLeftVelocity -= speedLeft;
+            backRightVelocity += 0;
+        } else { //no direction
+            frontLeftVelocity = frontLeftVelocity * 2; //multiply velocity by 2 to speed up stationary rotation
+            frontRightVelocity = frontRightVelocity * 2;
+            backLeftVelocity = backLeftVelocity * 2;
+            backRightVelocity = backRightVelocity * 2;
+        }
+    }
 }
